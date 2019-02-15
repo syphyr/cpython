5d4d7d267af721cbc30c1813e61178b28fd1793e
Ubuntu Core Developers <ubuntu-devel-discuss@lists.ubuntu.com>
2019-02-14 18:59:14 -0800

    bdist-wininst-notfound

--- Lib/distutils/command/bdist_wininst.py
+++ Lib/distutils/command/bdist_wininst.py
@@ -370,7 +370,10 @@ class bdist_wininst(Command):
             sfix = ''
 
         filename = os.path.join(directory, "wininst-%s%s.exe" % (bv, sfix))
-        f = open(filename, "rb")
+        try:
+            f = open(filename, "rb")
+        except IOError as e:
+            raise DistutilsFileError(str(e) + ', %s not included in the Debian packages.' % filename)
         try:
             return f.read()
         finally:
