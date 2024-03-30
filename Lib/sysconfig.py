5477b4f56a8b0250814006388ccc0b3288117746
Anthony Sottile <asottile@umich.edu>
2024-03-30 01:53:23 +0100

    sysconfigdata-name

--- Lib/sysconfig.py
+++ Lib/sysconfig.py
@@ -353,9 +353,8 @@ def get_makefile_filename():
 
 def _get_sysconfigdata_name():
     return os.environ.get('_PYTHON_SYSCONFIGDATA_NAME',
-        '_sysconfigdata_{abi}_{platform}_{multiarch}'.format(
+        '_sysconfigdata_{abi}_{multiarch}'.format(
         abi=sys.abiflags,
-        platform=sys.platform,
         multiarch=getattr(sys.implementation, '_multiarch', ''),
     ))
 
