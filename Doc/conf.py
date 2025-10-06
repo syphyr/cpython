dde71f90902e3cee8a1b8ffee319240ffd433547
syphyr <syphyr@gmail.com>
2025-10-06 03:23:38 +0200

    sphinx3

--- Doc/conf.py
+++ Doc/conf.py
@@ -280,6 +280,10 @@ gettext_additional_targets = [
     'index',
 ]
 
+# Allow to build with Sphinx >= 3.2
+c_allow_pre_v3 = True
+c_warn_on_allowed_pre_v3 = False
+
 # Options for HTML output
 # -----------------------
 
